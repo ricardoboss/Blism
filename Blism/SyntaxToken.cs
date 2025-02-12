b82257aa2723039f2101ab51c4bda6fff81d6336
Ricardo Boss <mail@ricardoboss.de>
2025-02-12 23:13:48 +0100

    Added test for base tokenizer

--- Blism/SyntaxToken.cs
+++ Blism/SyntaxToken.cs
@@ -5,4 +5,6 @@ public class SyntaxToken<TTokenType> where TTokenType : Enum
 	public required string Value { get; init; }
 
 	public required TTokenType Type { get; init; }
+
+	public override string ToString() => $"<{Type}>{Value}";
 }
