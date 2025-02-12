56ad8a120932848a949ec8ccb339602489a42ea6
Ricardo Boss <mail@ricardoboss.de>
2025-02-12 23:31:16 +0100

    Added test for SyntaxHighlighter component

---
+++ Blism.Test/BunitTestContext.cs
@@ -0,0 +1,12 @@
+﻿using Bunit;
+
+namespace Blism.Test;
+
+public abstract class BunitTestContext : TestContextWrapper
+{
+	[SetUp]
+	public void Setup() => TestContext = new();
+
+	[TearDown]
+	public void TearDown() => TestContext?.Dispose();
+}
---
+++ Blism.Test/SimpleHighlighter.cs
@@ -0,0 +1,21 @@
+﻿namespace Blism.Test;
+
+public class SimpleHighlighter : ITokenTypeHighlighter<SimpleTokenType>
+{
+	public string GetCss(SimpleTokenType tokenType)
+	{
+		return tokenType switch
+		{
+			SimpleTokenType.Char => "color: blue;",
+			SimpleTokenType.Digit => "color: green;",
+			SimpleTokenType.Keyword => "color: purple;",
+			SimpleTokenType.Unknown => "color: yellow;",
+			_ => "",
+		};
+	}
+
+	public string GetDefaultCss()
+	{
+		return "color: red;";
+	}
+}
