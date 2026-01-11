341a97279baf6e710c32b6443f47ee9b4d187b50
Ricardo Boss <mail@ricardoboss.de>
2026-01-11 01:53:27 +0100

    feat: added WPF engine and implemented syntax highlighters for Bash and C#

---
+++ Languages/Bash/Blism.Language.Bash.Wpf/BashSyntaxHighlighter.cs
@@ -0,0 +1,13 @@
+﻿using Blism.Language.Bash.Core;
+using Blism.Wpf;
+
+namespace Blism.Language.Bash.Wpf;
+
+public class BashSyntaxHighlighter : SyntaxHighlighter<BashTokenType>
+{
+	public BashSyntaxHighlighter()
+	{
+		Tokenizer = BashTokenizer.Instance;
+		StyleMapper = BashTokenStyleMapper.Instance;
+	}
+}
